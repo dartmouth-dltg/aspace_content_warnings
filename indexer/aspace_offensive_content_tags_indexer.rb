45e71b10857d4ed77b530cda4d0362d668c30516
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2021-07-12 17:35:16 -0400

    add additional indexer entries for ancestors

--- indexer/aspace_offensive_content_tags_indexer.rb
+++ indexer/aspace_offensive_content_tags_indexer.rb
@@ -12,6 +12,25 @@ class IndexerCommon
             doc['offensive_content_tags_u_sstr'] << I18n.t('enumerations.offensive_content_tag_code.' + oct['offensive_content_tag_code'])
           end
         end
+      
+        # for archival objects, check up the ancestor tree to see if any ancestors have applied any offensive content tags
+        if doc['primary_type'] == 'archival_object' && record['record']['offensive_content_tags'].empty?
+          doc['ancestor_applied_offensive_tags_u_sstr'] = []
+          doc['ancestor_applied_offensive_tags_at_level_u_sstr'] = ''
+          if record['record']['ancestors']
+            record['record']['ancestors'].each do |anc|
+              if doc['ancestor_applied_offensive_tags_at_level_u_sstr'].empty?
+                res_anc = JSONModel::HTTP.get_json(anc['ref'])
+                next if res_anc['offensive_content_tags'].empty?
+                res_anc['offensive_content_tags'].each do |oct|
+                  doc['ancestor_applied_offensive_tags_u_sstr'] << I18n.t('enumerations.offensive_content_tag_code.' + oct['offensive_content_tag_code'])
+                end
+                doc['ancestor_applied_offensive_tags_at_level_u_sstr'] = res_anc['level']
+              end
+            end
+          end
+          
+        end
       }
     end
   end
