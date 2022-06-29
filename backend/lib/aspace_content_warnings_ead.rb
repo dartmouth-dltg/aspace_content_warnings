10fe09129b2215a0281b55b235e0bf1d1dc22d71
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-06-29 09:55:28 -0400

    Note Type & Digital Object Export
    - allow for scopecontent or odd as EAD/EAD3 note type
    - include digital objects harmful content in EAD/EAD3 & PDF (staff)

--- backend/lib/aspace_content_warnings_ead.rb
+++ backend/lib/aspace_content_warnings_ead.rb
@@ -77,7 +77,7 @@ class EADSerializer < ASpaceExport::Serializer
           }# </did>
 
           # This is it. The patch. All one lines of it
-          serialize_aspace_content_warnings_ead(data, xml, @fragments)
+          HarmfulContentEAD.serialize_aspace_content_warnings(data, xml, @fragments, EADSerializer)
           # end the patch
 
           data.digital_objects.each do |dob|
@@ -180,7 +180,7 @@ class EADSerializer < ASpaceExport::Serializer
       }
 
       # This is it. The patch. All one line of it for AOs
-      serialize_aspace_content_warnings_ead(data, xml, @fragments)
+      HarmfulContentEAD.serialize_aspace_content_warnings(data, xml, @fragments, EADSerializer)
       # end the patch
 
       serialize_nondid_notes(data, xml, fragments)
@@ -203,35 +203,74 @@ class EADSerializer < ASpaceExport::Serializer
     }
   end
 
-  # custom method to include Local Contexts data
-  def serialize_aspace_content_warnings_ead(data, xml, fragments)
-    if AppConfig[:plugins].include?('aspace_content_warnings')
-      if data.content_warnings && data.content_warnings.length > 0
-        xml.odd {
-          xml.head {
-            sanitize_mixed_content(I18n.t("content_warning.section_title") , xml, fragments)
-          }
-          if AppConfig.has_key?(:aspace_content_warnings) && AppConfig[:aspace_content_warnings]['general_only'] == true
-            cw_text = I18n.t("enumerations.content_warning_code.cw_general") + ": " + I18n.t("content_warning_description.cw_general")
-            xml.p {
-              sanitize_mixed_content(cw_text , xml, fragments)
-            }
-          else
-            data.content_warnings.each do |cw|
-              cw_type = cw['content_warning_code']
-              if cw['description']
-                cw_description = cw['description']
-              else
-                cw_description = I18n.t("content_warning_description.#{cw_type}")
-              end
-              cw_text = I18n.t("enumerations.content_warning_code.#{cw_type}") + ": " + cw_description
-              xml.p {
-                sanitize_mixed_content(cw_text , xml, fragments)
-              }
-            end
-          end
-          }
+  # Override this method to include Harmful Content data
+  def serialize_digital_object(digital_object, xml, fragments)
+    return if digital_object["publish"] === false && !@include_unpublished
+    return if digital_object["suppressed"] === true
+
+    # ANW-285: Only serialize file versions that are published, unless include_unpublished flag is set
+    file_versions_to_display = digital_object['file_versions'].select {|fv| fv['publish'] == true || @include_unpublished }
+
+    title = digital_object['title']
+    date = digital_object['dates'][0] || {}
+
+    atts = {}
+
+    content = ""
+    content << title if title
+    content << ": " if date['expression'] || date['begin']
+    if date['expression']
+      content << date['expression']
+    elsif date['begin']
+      content << date['begin']
+      if date['end'] != date['begin']
+        content << "-#{date['end']}"
       end
+    end
+
+    # Harmful Content start
+    content << HarmfulContentEAD.serialize_aspace_content_warnings_for_digital_objects(digital_object)
+    # Harmful Content end
+
+    atts['xlink:title'] = digital_object['title'] if digital_object['title']
+
+
+    if file_versions_to_display.empty?
+      atts['xlink:type'] = 'simple'
+      atts['xlink:href'] = digital_object['digital_object_id']
+      atts['xlink:actuate'] = 'onRequest'
+      atts['xlink:show'] = 'new'
+      atts['audience'] = 'internal' unless is_digital_object_published?(digital_object)
+      xml.dao(atts) {
+        xml.daodesc { sanitize_mixed_content(content, xml, fragments, true) } if content
+      }
+    elsif file_versions_to_display.length == 1
+      file_version = file_versions_to_display.first
+
+      atts['xlink:type'] = 'simple'
+      atts['xlink:actuate'] = file_version['xlink_actuate_attribute'] || 'onRequest'
+      atts['xlink:show'] = file_version['xlink_show_attribute'] || 'new'
+      atts['xlink:role'] = file_version['use_statement'] if file_version['use_statement']
+      atts['xlink:href'] = file_version['file_uri']
+      atts['audience'] = 'internal' unless is_digital_object_published?(digital_object, file_version)
+      xml.dao(atts) {
+        xml.daodesc { sanitize_mixed_content(content, xml, fragments, true) } if content
+      }
+    else
+      atts['xlink:type'] = 'extended'
+      atts['audience'] = 'internal' unless is_digital_object_published?(digital_object)
+      xml.daogrp( atts ) {
+        xml.daodesc { sanitize_mixed_content(content, xml, fragments, true) } if content
+        file_versions_to_display.each do |file_version|
+          atts = {}
+          atts['xlink:type'] = 'locator'
+          atts['xlink:href'] = file_version['file_uri']
+          atts['xlink:role'] = file_version['use_statement'] if file_version['use_statement']
+          atts['xlink:title'] = file_version['caption'] if file_version['caption']
+          atts['audience'] = 'internal' unless is_digital_object_published?(digital_object, file_version)
+          xml.daoloc(atts)
+        end
+      }
     end
   end
 
@@ -318,7 +357,7 @@ class EAD3Serializer < EADSerializer
             }# </did>
 
             # This is it. The patch. All one line of it
-            serialize_aspace_content_warnings_ead3(data, xml, @fragments)
+            HarmfulContentEAD.serialize_aspace_content_warnings(data, xml, @fragments, EAD3Serializer)
             # end the patch
 
             serialize_nondid_notes(data, xml, @fragments)
@@ -431,7 +470,7 @@ class EAD3Serializer < EADSerializer
         }
 
         # This is it. The patch. All one line of it for AOs
-        serialize_aspace_content_warnings_ead3(data, xml, @fragments)
+        HarmfulContentEAD.serialize_aspace_content_warnings(data, xml, @fragments, EAD3Serializer)
         # end the patch
 
         serialize_nondid_notes(data, xml, fragments)
@@ -455,36 +494,128 @@ class EAD3Serializer < EADSerializer
     end
   end
 
-  # custom method to include Local Contexts data
-  def serialize_aspace_content_warnings_ead3(data, xml, fragments)
+  # Override this method to include Harmful Content data
+  def serialize_digital_object(digital_object, xml, fragments)
+    return if digital_object["publish"] === false && !@include_unpublished
+    return if digital_object["suppressed"] === true
+
+    file_versions = digital_object['file_versions']
+    title = digital_object['title']
+    date = digital_object['dates'][0] || {}
+
+    atts = {}
+
+    content = ""
+    content << title if title
+    content << ": " if date['expression'] || date['begin']
+    if date['expression']
+      content << date['expression']
+    elsif date['begin']
+      content << date['begin']
+      if date['end'] != date['begin']
+        content << "-#{date['end']}"
+      end
+    end
+
+    # Harmful Content start
+    content << HarmfulContentEAD.serialize_aspace_content_warnings_for_digital_objects(digital_object)
+    # Harmful Content end
+
+    atts['linktitle'] = digital_object['title'] if digital_object['title']
+
+    if digital_object['digital_object_type']
+      atts['daotype'] = 'otherdaotype'
+      atts['otherdaotype'] = digital_object['digital_object_type']
+    else
+      atts['daotype'] = 'unknown'
+    end
+
+    if file_versions.empty?
+      atts['href'] = digital_object['digital_object_id']
+      atts['actuate'] = 'onrequest'
+      atts['show'] = 'new'
+      atts['audience'] = 'internal' unless is_digital_object_published?(digital_object)
+      xml.dao(atts) {
+        xml.descriptivenote { sanitize_mixed_content(content, xml, fragments, true) } if content
+      }
+    else
+      file_versions.each do |file_version|
+        atts['href'] = file_version['file_uri'] || digital_object['digital_object_id']
+        atts['actuate'] = (file_version['xlink_actuate_attribute'].respond_to?(:downcase) && file_version['xlink_actuate_attribute'].downcase) || 'onrequest'
+        atts['show'] = (file_version['xlink_show_attribute'].respond_to?(:downcase) && file_version['xlink_show_attribute'].downcase) || 'new'
+        atts['localtype'] = file_version['use_statement'] if file_version['use_statement']
+        atts['audience'] = 'internal' unless is_digital_object_published?(digital_object, file_version)
+        xml.dao(atts) {
+          xml.descriptivenote { sanitize_mixed_content(content, xml, fragments, true) } if content
+        }
+      end
+    end
+  end
+
+end
+
+class HarmfulContentEAD
+
+  def self.serialize_aspace_content_warnings(data, xml, fragments, ead_serializer_class)
     if AppConfig[:plugins].include?('aspace_content_warnings')
+      ead_serializer_caller = ead_serializer_class.new
       if data.content_warnings && data.content_warnings.length > 0
-        xml.odd {
+        xml.send(AppConfig[:aspace_content_warnings_note_type]) {
           xml.head {
-            sanitize_mixed_content(I18n.t("content_warning.section_title") , xml, fragments)
+            ead_serializer_caller.sanitize_mixed_content(I18n.t("content_warning.section_title") , xml, fragments)
           }
           if AppConfig.has_key?(:aspace_content_warnings) && AppConfig[:aspace_content_warnings]['general_only'] == true
-            cw_text = I18n.t("enumerations.content_warning_code.cw_general") + ": " + I18n.t("content_warning_description.cw_general")
+            cw_text = general_cw_text
             xml.p {
-              sanitize_mixed_content(cw_text , xml, fragments)
+              ead_serializer_caller.sanitize_mixed_content(cw_text , xml, fragments)
             }
           else
             data.content_warnings.each do |cw|
-              cw_type = cw['content_warning_code']
-              if cw['description']
-                cw_description = cw['description']
-              else
-                cw_description = I18n.t("content_warning_description.#{cw_type}")
-              end
-              cw_text = I18n.t("enumerations.content_warning_code.#{cw_type}") + ": " + cw_description
               xml.p {
-                sanitize_mixed_content(cw_text , xml, fragments)
+                ead_serializer_caller.sanitize_mixed_content(assemble_content_warning_text(cw) , xml, fragments)
               }
             end
           end
           }
       end
     end
+  end
+
+  def self.serialize_aspace_content_warnings_for_digital_objects(digital_object)
+    if AppConfig[:plugins].include?('aspace_content_warnings')
+      if digital_object['content_warnings'] && digital_object['content_warnings'].length > 0
+        content = "; "
+        content << I18n.t("content_warning.section_title")
+        content << ". "
+        if AppConfig.has_key?(:aspace_content_warnings) && AppConfig[:aspace_content_warnings]['general_only'] == true
+          content << general_cw_text
+        else
+          digital_object['content_warnings'].each_with_index do |cw, idx|
+            content << assemble_content_warning_text(cw)
+            unless idx == digital_object['content_warnings'].length - 1
+              content << ", "
+            end
+          end
+        end
+      end
+      content
+    end
+  end
+
+  private
+
+  def self.general_cw_text
+    cw_text = I18n.t("enumerations.content_warning_code.cw_general") + " - " + I18n.t("content_warning_description.cw_general")
+  end
+
+  def self.assemble_content_warning_text(cw)
+    cw_type = cw['content_warning_code']
+    if cw['description']
+      cw_description = cw['description']
+    else
+      cw_description = I18n.t("content_warning_description.#{cw_type}")
+    end
+    cw_text = I18n.t("enumerations.content_warning_code.#{cw_type}") + " - " + cw_description
   end
 
 end
