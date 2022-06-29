80ddc2c6f58556883f3f1d04432b46cb2ce101fc
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-06-29 09:57:07 -0400

    Note Type & Digital Object Export
    - allow for scopecontent or odd as EAD/EAD3 note type
    - include digital objects harmful content in EAD/EAD3 & PDF (staff)

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,1 +1,10 @@
 require_relative 'lib/aspace_content_warnings_ead'
+
+# allow for odd or scopecontent as the EAD/EAD3 note type
+if AppConfig.has_key?(:aspace_content_warnings_note_type)
+  unless ['odd', 'scopcontent'].include?(AppConfig[:aspace_content_warnings_note_type])
+    AppConfig[:aspace_content_warnings_note_type] = 'odd'
+  end
+else
+  AppConfig[:aspace_content_warnings_note_type] = 'odd'
+end
