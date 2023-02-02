852cf0f5d4b6cfe67acf937914e168f036f3092c
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2023-02-02 09:20:08 -0500

    typo

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -9,7 +9,7 @@ require_relative 'lib/aspace_content_warnings_marc_serialize'
 
 # allow for odd or scopecontent as the EAD/EAD3 note type
 if AppConfig.has_key?(:aspace_content_warnings_note_type)
-  unless ['odd', 'scopcontent'].include?(AppConfig[:aspace_content_warnings_note_type])
+  unless ['odd', 'scopecontent'].include?(AppConfig[:aspace_content_warnings_note_type])
     AppConfig[:aspace_content_warnings_note_type] = 'scopecontent'
   end
 else
