fc6230bc657f0f37cc5545bdac7d066e2e369aa6
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2024-09-30 13:03:35 -0400

    fixes for AS 4.0

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,15 +1,10 @@
 ArchivesSpace::Application.config.after_initialize do
   # only add faceting if configured
   if AppConfig.has_key?(:aspace_content_warnings) && AppConfig[:aspace_content_warnings]['staff_faceting'] == true
-    SearchResultData.class_eval do
-      self.singleton_class.send(:alias_method, :BASE_FACETS_pre_content_warnings, :BASE_FACETS)
-      def self.BASE_FACETS
-        if AppConfig[:aspace_content_warnings]['general_only'] == true
-          self.BASE_FACETS_pre_content_warnings << "content_warnings_general_u_sbool"
-        else
-          self.BASE_FACETS_pre_content_warnings << "content_warnings_u_sstr"
-        end
-      end
+    if AppConfig[:aspace_content_warnings]['general_only'] == true
+      Plugins::add_search_base_facets('content_warnings_general_u_sbool')
+    else
+      Plugins::add_search_base_facets('content_warnings_u_sstr')
     end
   end
 
